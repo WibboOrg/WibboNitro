d1193b01aaecc26b9aec0d5dff5b3634720cd34f
Kodamas <superjason95@gmail.com>
2023-12-14 02:13:17 +0100

    Update Slider.tsx

--- nitro-react/src/common/Slider.tsx
+++ nitro-react/src/common/Slider.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import ReactSlider, { ReactSliderProps } from 'react-slider';
 import { Button } from './Button';
 import { Flex } from './Flex';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 
 export interface SliderProps extends ReactSliderProps
 {
@@ -13,8 +14,8 @@ export const Slider: FC<SliderProps> = props =>
     const { disabledButton, max, min, value, onChange, ...rest } = props;
 
     return <Flex fullWidth gap={ 1 }>
-        { !disabledButton && <Button disabled={ min >= value } onClick={ () => onChange(min < value ? value - 1 : min, 0) }>&lt;</Button> }
+        { !disabledButton && <Button disabled={ min >= value } onClick={ () => onChange(min < value ? value - 1 : min, 0) }><FaAngleLeft /></Button> }
         <ReactSlider className={ 'nitro-slider' } max={ max } min={ min } value={ value } onChange={ onChange } { ...rest } />
-        { !disabledButton && <Button disabled={ max <= value } onClick={ () => onChange(max > value ? value + 1 : max, 0) }>&gt;</Button> }
+        { !disabledButton && <Button disabled={ max <= value } onClick={ () => onChange(max > value ? value + 1 : max, 0) }><FaAngleRight /></Button> }
     </Flex>;
 }
