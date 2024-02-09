0dcf3dcdb0ad72e7e29bc4dc9215bdb005f1d7c3
Kodamas <superjason95@gmail.com>
2024-02-09 22:52:03 +0100

    fix: get local storage

--- nitro-react/src/api/utils/GetLocalStorage.ts
+++ nitro-react/src/api/utils/GetLocalStorage.ts
@@ -3,7 +3,7 @@ export const GetLocalStorage = <T>(key: string) =>
     try
     {
         const value = window.localStorage.getItem(key);
-        return JSON.parse(value) as T ?? value
+        return JSON.parse(value) as T ?? value as T
     }
     catch(e)
     {
